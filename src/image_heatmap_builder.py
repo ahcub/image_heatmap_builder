99b57bfc515a16798c6326dbb919b2db59db2eff
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2015-12-12 14:29:18 +0200

    !U restructure files

---
+++ src/image_heatmap_builder.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt4 import QtGui
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    main_window = QtGui.QWidget()
+
+    main_layout = QtGui.QVBoxLayout()
+
--- utils.py
+++
@@ -1,2 +0,0 @@
-
-def build_heatmap_on_image()
